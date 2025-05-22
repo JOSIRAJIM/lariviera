06c982e7dd2f9425233d6c1a73b0f4d237b273e2
JOSIRAJIM <30900836+JOSIRAJIM@users.noreply.github.com>
2025-05-21 21:42:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import cross_val_score
 from procesamiento import cargar_y_preprocesar_datos
