1225a68f2b01610a166a0ff8e9863862fc0c4a6a
JOSIRAJIM <30900836+JOSIRAJIM@users.noreply.github.com>
2025-05-21 21:46:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import cross_val_score
